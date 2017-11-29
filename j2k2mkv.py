7201c3e60a4513582649a5eb5cdaed637bfd57b7
FutureDays <futuredaysmedia@gmail.com>
2017-11-29 08:51:38 -0800

    updates

--- j2k2mkv.py
+++ j2k2mkv.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env/python
 import os
+import re
+import sys
+import argparse
 import subprocess
 
 class cd:
@@ -43,8 +47,73 @@ def probe_streams(obj):
         print _out[1]
         return False
 
+def go(ffstr):
+	'''
+	runs ffmpeg, returns true is success, error is fail
+	'''
+	try:
+		returncode = subprocess.check_output(ffstr)
+		returncode = True
+	except subprocess.CalledProcessError, e:
+		returncode = e.returncode
+		print returncode
+	return returncode
+
+def process(kwargs):
+    '''
+    processes a single file
+    '''
+    print "Processing " + kwargs.input.fullpath
+    kwargs.input.fname, kwargs.input.ext = os.path.splitext(os.path.basename(kwargs.input.fullpath))
+    kwargs.input.dirname = os.path.dirname(kwargs.input.fullpath)
+    if kwargs.output is None:
+        kwargs.output = dotdict({"fullpath":os.path.join(kwargs.input.dirname, kwargs.input.fname, ".mkv")})
+    elif os.path.isdir(kwargs.output):
+        dirname = kwargs.output
+        kwargs.output = dotdict({"dirname":dirname, "fname":kwargs.input.fname})
+        kwargs.output.fullpath = os.path.join(kwargs.output.dirname, kwargs.output.fname, ".mkv")
+    else:
+        kwargs.output = dotdict({"fullpath"})
+    return True
+
+def init_args():
+    '''
+    initialize arguments from CLI
+    '''
+    parser = argparse.ArgumentParser(description="transcodes from j2k to ffvi")
+    #parser.add_argument('-m',dest='m',choices=['batch','single'],default=False,help='mode, for processing a single transfer or a batch in new_ingest')
+    parser.add_argument('-i', '--input', dest='i', help="the input filepath")
+    parser.add_argument('-o', '--output', dest='o', default=None, help="the output path")
+    args = parser.parse_args()
+    args.i = args.i.replace("\\", "/")
+    args.o = args.o.replace("\\", "/")
+    return args
+
 def main():
-    print "foo"
+    '''
+    NTSC
+    ffmpeg -i input_file -map 0 -dn -c:v ffv1 -level 3 -g 1 -slicecrc 1 -slices 24 -field_order bb -vf setfield=bff,setdar=4/3 -color_primaries smpte170m -color_trc bt709 -colorspace smpte170m -color_range mpeg -c:a copy output_file.mkv
+    PAL
+    ffmpeg -i input_file -map 0 -dn -c:v ffv1 -level 3 -g 1 -slicecrc 1 -slices 24 -field_order bt -vf setfield=tff,setdar=4/3 -color_primaries bt470bg -color_trc bt709 -colorspace bt470bg  -color_range mpeg -c:a copy output_file.mkv
+    '''
+    args = init_args()
+    if os.path.isdir(args.i):
+        for dirs, subdirs, files in os.walk(args.i):
+            for f in files:
+                itWorked = process(dotdict({"input":dotdict({"fullpath":os.path.join(dirs, f)}), "output":args.o}))
+    else:
+        match = ''
+        match = re.search(r"[aA-zZ]/:", args.i)
+        if match or args.i.startswith("/"):
+            fp = args.i
+        else:
+            fp = os.path.join(os.getcwd(), args.i)
+        if not os.path.exists(fp):
+            print "Buddy, we couldn't find the file"
+            print "Please check that it exists at:"
+            print fp
+            sys.exit()
+        itWorked = process(dotdict({"input":dotdict({"fullpath":fp}), "output":args.o}))
 
 if __name__ == '__main__':
     main()
